97ac7f0aa350e555f586ff6b2e80bb9607f4f26d
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2024-11-14 12:01:45 +0000

    final

---
+++ network.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function create_network {
+  ansible-playbook Virtualswitch/playbook.yaml \
+                --extra-vars "@Virtualswitch/variables.yaml" \
+                --extra-vars "@config/connection.yaml" \
+                --extra-vars "@config/secretos.yaml" \
+                --extra-vars "state=present"
+  ansible-playbook Network/playbook.yaml \
+                --extra-vars "@Network/variables.yaml" \
+                --extra-vars "@Virtualswitch/variables.yaml" \
+                --extra-vars "@config/connection.yaml" \
+                --extra-vars "@config/secretos.yaml" \ 
+                --extra-vars "state=present"
+}
+
+function destroy_network {
+  ansible-playbook Network/playbook.yaml \
+                --extra-vars "@Network/variables.yaml" \
+                --extra-vars "@Virtualswitch/variables.yaml" \
+                --extra-vars "@config/connection.yaml" \
+                --extra-vars "@config/secretos.yaml" \ 
+                --extra-vars "state=absent"
+  ansible-playbook Virtualswitch/playbook.yaml \
+                --extra-vars "@Virtualswitch/variables.yaml" \
+                --extra-vars "@config/connection.yaml" \
+                --extra-vars "@config/secretos.yaml" \
+                --extra-vars "state=absent"
+}
+
+[[ "$1" == "delete" ]] && destroy_network || create_network
